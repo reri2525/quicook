eddb59821df8fb22622663aaf20975e8d69381fb
revali2525 <0831yuuki@i.softbank.jp>
2023-08-23 02:09:02 +0900

    update

--- front/start.sh
+++ front/start.sh
@@ -3,4 +3,8 @@ cd app
 if [ "$REACT_APP_ENV" = "production" ]; then
     npm run build
     npm start
+else
+    rm -rf /usr/src/app/app/node_modules/.cache
+    mkdir -p /usr/src/app/app/node_modules/.cache
+    chown -R node:node /usr/src/app/app/node_modules/.cache
 fi
