a91e2f11ca0a0394ac84c22fbee676e957ffef92
revali2525 <0831yuuki@i.softbank.jp>
2023-08-27 15:39:43 +0900

    update

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
+    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:migrate:reset RAILS_ENV=production
+    rails db:seed RAILS_ENV=production
     bundle exec pumactl start
 elif [ "$RAILS_ENV" = "development" ]; then
     ls
